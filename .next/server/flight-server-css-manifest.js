85097abe3eaf9f97bce67b9b6b93b4d1953b01a4
= <=>
2023-05-23 17:49:13 +0100

    Added storybook

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "D:\\Projects\\Projects 2023\\games\\src\\app\\layout.tsx": [
+      "D:\\Projects\\Projects 2023\\games\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "D:\\Projects\\Projects 2023\\games\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "D:\\Projects\\Projects 2023\\games\\src\\app\\jackpot\\page": [
+      "D:\\Projects\\Projects 2023\\games\\src\\app\\globals.css",
+      "D:\\Projects\\Projects 2023\\games\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}

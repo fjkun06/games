ecea30db3bc0570676d980aa232865c65163489f
= <=>
2023-05-23 23:06:52 +0100

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "../styles/main.scss";
 import "@fontsource/roboto";
 import React from "react";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import Footer from "@/stories/Footer";
 export const metadata = {
   title: "Games",
   description: "Game application",
@@ -14,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   const lg = useMediaQuery("(width > 840px)");
   const [isOpen, setIsOpen] = React.useState(false);
   const toggleNavbar = () => setIsOpen(!isOpen);
-  
+
   //display navbar items horisontally when above 840px width
   React.useEffect(() => {
     if (lg) {
@@ -41,7 +42,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <Navbar handleClick={toggleNavbar} isOpen={isOpen} />
-        <main onClick={() => setIsOpen(false)}>{children}</main>
+        <main id="container" onClick={() => setIsOpen(false)}>{children}</main>
+        <Footer />
       </body>
     </html>
   );
